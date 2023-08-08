7f00dc551ba2a068af88a7486e057ccae1807c5d
khanmustuffa11 <khanmustuffa011@gmail.com>
2023-08-08 15:07:40 +0530

    oct code on vit , convnext, from machine

--- predict_convnext.py
+++ predict_convnext.py
@@ -7,22 +7,61 @@ import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 import glob
 import os
-# os.environ["CUDA_VISIBLE_DEVICES"]="0"
-#df = pd.read_csv('final_test.csv')
-df = pd.read_csv('data/oct/oct_artelus/OCT_final_testwcsv.csv')
-#df=df[df['source']=='idrid']
-#model = load_model('oct_mendely_artelus_models\model_artmen.h5', compile=False)
-#my_reloaded_model = tf.keras.models.load_model('model_convnext031--0.382818--0.985649--0.420023--0.965179.h5',custom_objects={'LayerScale':hub.LayerScale})
-model = load_model('model_convnext031--0.382818--0.985649--0.420023--0.965179.h5', compile=False)
-print(model)
+import random
+import numpy as np
+from tqdm import tqdm
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model, load_model, Sequential
+from tensorflow.keras.layers import Dense,GlobalAveragePooling2D, Flatten, Dropout
+#import tensorflow_addons as tfa
+from tensorflow.keras.optimizers import Adam
+from sklearn.utils import class_weight
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, Callback, TensorBoard
+import tensorflow as tf
+from PIL import Image, ImageFile
+from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
+import pandas as pd
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+from tensorflow.keras.metrics import top_k_categorical_accuracy
+from sklearn.utils import shuffle
+#from tensorflow.keras.applications.efficientnet_v2 import EfficientNetV2B0
+import os
+import sklearn.metrics as sklm
+from sklearn.utils.class_weight import compute_sample_weight
+#from tensorflow_addons.metrics import F1Score
+from convnext import ConvNeXtTiny
+
+# Define the custom layer
+class LayerScale(tf.keras.layers.Layer):
+    pass
+    # Your implementation here...
+
+# Register the custom layer
+tf.keras.utils.get_custom_objects()['LayerScale'] = LayerScale
+
+# Load the model with the custom layer
+base_model = ConvNeXtTiny(weights='imagenet',include_top=False, pooling = 'avg')
+model = Sequential()
+model.add(base_model)
+model.add(Dropout(.000000000005))
+model.add(Dense(8, activation = 'softmax'))
+model.load_weights('model_convnext_finetune021--0.492148--0.991566--0.589796--0.962033.h5')
+
+#model = load_model('model_convnext031--0.382818--0.985649--0.420023--0.965179.h5', compile=False, custom_objects={'LayerScale': LayerScale})
+#print(model)
+
 def get_image(file_path):
     img = Image.open(file_path).convert('RGB')
-    img = np.asarray(img.resize((500,500)))
+    img = np.asarray(img.resize((224,224)))
     img = tf.cast(img, tf.float32)
     img = np.expand_dims(img, axis=0)
     return img
 
-## CSV Prediction ##
+# Load the CSV data
+df = pd.read_csv('data/oct/oct_artelus/OCT_final_testwcsv.csv')
+
+# Perform predictions
 predictions = []
 confidence = []
 all_pred = []
@@ -30,60 +69,9 @@ for i in tqdm(range(len(df))):
     img_path = df['path'].iloc[i]
     pred = model.predict(get_image(img_path))[0]
     all_pred.append([img_path, np.argmax(np.asarray(pred)), max(pred)])
-    # predictions.append(np.argmax(np.asarray(pred)))
-    # confidence.append(max(pred))
-df = pd.DataFrame(all_pred, columns=['image','predictions','confidence'])
-df.to_csv('convnext_results_7july.csv')
-### END ###
 
-# import tensorflow as tf
-# from tqdm import tqdm
-# import pandas as pd
-# from tensorflow.keras.models import load_model
-# from PIL import Image
-# import numpy as np
-# from sklearn.metrics import classification_report, confusion_matrix
-# import glob
-# import os
+# Create DataFrame from predictions
+df_pred = pd.DataFrame(all_pred, columns=['image', 'predictions', 'confidence'])
 
-# # Define the custom layer
-# class LayerScale(tf.keras.layers.Layer):
-#     pass
-#     # Your implementation here...
-
-# # Register the custom layer
-# tf.keras.utils.get_custom_objects()['LayerScale'] = LayerScale
-
-# # Load the model with the custom layer
-# model = load_model('model_convnext031--0.382818--0.985649--0.420023--0.965179.h5', compile=False, custom_objects={'LayerScale': LayerScale})
-# print(model)
-
-# def get_image(file_path):
-#     img = Image.open(file_path).convert('RGB')
-#     img = np.asarray(img.resize((500,500)))
-#     img = tf.cast(img, tf.float32)
-#     img = np.expand_dims(img, axis=0)
-#     return img
-
-# # Load the CSV data
-# df = pd.read_csv('data/oct/oct_artelus/OCT_final_testwcsv.csv')
-
-# # Perform predictions
-# predictions = []
-# confidence = []
-# all_pred = []
-# for i in tqdm(range(len(df))):
-#     img_path = df['path'].iloc[i]
-#     pred = model.predict(get_image(img_path))[0]
-#     all_pred.append([img_path, np.argmax(np.asarray(pred)), max(pred)])
-
-# # Create DataFrame from predictions
-# df_pred = pd.DataFrame(all_pred, columns=['image', 'predictions', 'confidence'])
-
-# # Save predictions to CSV file
-# df_pred.to_csv('convnext_results_7july.csv')
-
-
-
-
-
+# Save predictions to CSV file
+df_pred.to_csv('convnext_results_7july.csv')
---
+++ vit_oct_xray_base.py
@@ -0,0 +1,215 @@
+import torchvision, torch
+from torchvision import transforms
+import os
+import math
+import random
+import numpy as np
+from PIL import Image
+from sklearn.utils import shuffle
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from sklearn.utils.class_weight import compute_sample_weight
+device = 'cuda'
+img_width, img_height= 224, 224
+BATCH_SIZE = 128
+DISEASE_LABELS = ['level_0', 'level_1','level_2','level_3','level_4','level_5','level_6','level_7']
+
+num_classes = len(DISEASE_LABELS)
+
+
+df_train, df_test = pd.read_csv('oct_final_train_vit.csv'), pd.read_csv('OCT_final_val_vitc.csv')
+
+
+def balance_df(dataframe, columns= DISEASE_LABELS):
+    label_dict = {}
+    max_sum = []
+    dataframe_out = dataframe
+    for col in columns:
+        label_dict[col] = dataframe[col].sum()
+        max_sum.append(dataframe[col].sum())
+    label_dict = dict(sorted(label_dict.items(), key=lambda item: item[1]))
+    for pathology,val in label_dict.items():
+        fraction = max((max(max_sum)/val - 3),0)
+        sampled_df = dataframe[dataframe[pathology] > 0.3]
+        upsampled_df = sampled_df.sample(frac=fraction, replace=True)
+        dataframe_out = pd.concat([dataframe_out, upsampled_df])
+    label_dict = {}
+    for col in columns:
+        label_dict[col] = dataframe_out[col].sum()
+    print('upsampling')
+    print(label_dict)
+    return dataframe_out
+
+df_train = balance_df(df_train)
+df_test = balance_df(df_test)
+
+def compute_weights(df,class_list=None):
+    weights = []
+    for each_class in class_list:
+        each_weight = list(compute_sample_weight(class_weight='balanced', y = df[each_class]))
+        weights.append(each_weight)
+    return np.max(np.array(weights),axis = 0)
+
+for i in range(100):
+    df_train = shuffle(df_train)
+
+def set_seeds(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+class Engine:
+    @staticmethod
+    def train(model, train_dataloader, test_dataloader, optimizer, loss_fn, epochs, device, save_dir="model_checkpoints"):
+        model.train()
+        results = {'train_losses': [], 'test_losses': [], 'train_accuracies': [], 'test_accuracies': []}
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        for epoch in range(epochs):
+            train_loss, train_correct, train_total = 0, 0, 0
+            for batch_idx, (inputs, labels) in enumerate(train_dataloader):
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = loss_fn(outputs, labels)
+
+                loss.backward()
+                optimizer.step()
+
+                train_loss += loss.item()
+
+                train_pred = torch.nn.functional.softmax(outputs,dim=1) > 0.5
+                train_correct += (train_pred == labels.byte()).sum().item()
+                train_total += labels.numel()
+                if batch_idx%100==0:
+                    print(f"Epoch {epoch + 1}/{epochs}, Batch {batch_idx + 1}/{len(train_dataloader)}, Loss: {loss.item():.4f}")
+
+            train_loss /= len(train_dataloader)
+            train_accuracy = train_correct / train_total
+            results['train_losses'].append(train_loss)
+            results['train_accuracies'].append(train_accuracy)
+
+            # Evaluate the model
+            model.eval()
+            test_loss, test_correct, test_total = 0, 0, 0
+            with torch.no_grad():
+                for inputs, labels in test_dataloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+
+                    outputs = model(inputs)
+                    loss = loss_fn(outputs, labels)
+
+                    test_loss += loss.item()
+
+                    test_pred = torch.nn.functional.softmax(outputs, dim=1) > 0.5
+                    test_correct += (test_pred == labels.byte()).sum().item()
+                    test_total += labels.numel()
+
+            test_loss /= len(test_dataloader)
+            test_accuracy = test_correct / test_total
+            results['test_losses'].append(test_loss)
+            results['test_accuracies'].append(test_accuracy)
+
+            # Save the model
+            model_path = f"{save_dir}/vit_oct_xrayre{train_loss:.4f}_{train_accuracy:.4f}_{test_loss:.4f}_{test_accuracy:.4f}.pth"
+            torch.save(model, model_path)
+
+            print(f"Epoch {epoch + 1}/{epochs}, Train Loss: {train_loss:.4f}, Train Acc: {train_accuracy:.4f}, Test Loss: {test_loss:.4f}, Test Acc: {test_accuracy:.4f}")
+
+        return results
+
+class CustomDataset(Dataset):
+    def __init__(self, data_frame, img_shape=None, augmentation=True, subset='training'):
+        self.data_frame = data_frame
+        self.img_shape = img_shape
+        self.augmentation = augmentation
+        self.subset = subset
+
+        print(f"Found {self.data_frame.shape[0]} images")
+
+    def __len__(self):
+        return len(self.data_frame)
+
+    def __getitem__(self, idx):
+        img_path = self.data_frame.iloc[idx]['path']
+        img = self.__get_image(img_path)
+        
+        labels = self.data_frame.iloc[idx][DISEASE_LABELS].values
+        labels = np.array(labels, dtype=np.float32)
+        labels = torch.tensor(labels, dtype=torch.float32)
+        return img, labels
+
+    def __data_augmentation(self, img):
+        img = Image.fromarray(img.astype('uint8'))
+        transform_list = [
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.7, saturation=0.7, hue=0.2),
+        transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=5),
+        transforms.RandomResizedCrop((img_width, img_height), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+        transforms.RandomRotation(degrees=15)
+    ]
+        transform = transforms.Compose(transform_list)
+        img = transform(img)
+        img = np.asarray(img)
+        return img
+
+    def __get_image(self, file_id):
+        #file_id = 'pathology_resized/' + str(file_id)
+        if self.subset != 'training':
+            img = Image.open(file_id).resize(self.img_shape).convert('RGB')
+            #img = transforms.ToTensor()(np.array(img))
+            img = np.asarray(img)
+            img = torch.from_numpy(img).permute(2, 0, 1).float()
+        else:
+            if random.randint(1, 5) == 5:
+                img = Image.open(file_id).convert('L').convert('RGB').resize(self.img_shape)
+                #img = transforms.ToTensor()(np.array(img))
+                img = np.asarray(img)
+                if self.augmentation:
+                    img = self.__data_augmentation(img)
+                img = torch.from_numpy(img).permute(2, 0, 1).float()
+            else:
+                img = Image.open(file_id).resize(self.img_shape).convert('RGB')
+                #img = transforms.ToTensor()(np.array(img))
+                img = np.asarray(img)
+                if self.augmentation:
+                    img = self.__data_augmentation(img)
+                img = torch.from_numpy(img).permute(2, 0, 1).float()
+        return img
+
+def create_dataloaders(train_df, test_df, img_shape, batch_size, num_workers=0):
+    train_dataset = CustomDataset(train_df, img_shape=img_shape, subset='training')
+    test_dataset = CustomDataset(test_df, img_shape=img_shape, subset='testing')
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_dataloader, test_dataloader
+
+train_dataloader, test_dataloader = create_dataloaders(df_train, df_test, img_shape=(img_width, img_height), batch_size=BATCH_SIZE)
+
+pretrained_vit = torch.load('model_checkpoints/vit_torch_xray0.3903_0.9243_0.4058_0.9182.pth')
+
+pretrained_vit.module.heads = torch.nn.Linear(in_features=pretrained_vit.module.heads.in_features, out_features=8)
+
+pretrained_vit = pretrained_vit.to('cuda')
+
+optimizer = torch.optim.Adam(pretrained_vit.parameters(), lr=0.001)
+criterion = torch.nn.BCEWithLogitsLoss()
+
+num_epochs = 100
+engine = Engine()
+set_seeds()
+pretrained_vit_results = engine.train(model=pretrained_vit,
+                                      train_dataloader=train_dataloader,
+                                      test_dataloader=test_dataloader,
+                                      optimizer=optimizer,
+                                      loss_fn=criterion,
+                                      epochs=num_epochs,
+                                      device=device)
+
